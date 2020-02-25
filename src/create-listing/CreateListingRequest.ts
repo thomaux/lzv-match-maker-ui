9c020600afb60b15514a281a0202d04208b3081d
Thomas Anciaux <thomas.anciaux@gmail.com>
2020-02-25 22:31:22 +0100

    Link actual date to request object

--- src/create-listing/CreateListingRequest.ts
+++ src/create-listing/CreateListingRequest.ts
@@ -6,8 +6,7 @@ export class CreateListingRequest {
     maxLevel: number;
 
     constructor(public readonly teamName: string, public readonly gymId: number, date: Date, levelRange: LevelRangeModel ) {
-        // TODO: map date input to date ISO string
-        this.date = '2020-12-31T22:00:00.000Z';
+        this.date = date.toISOString();
 
         this.minLevel = levelRange.min;
         this.maxLevel = levelRange.max
